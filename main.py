d6d17265ec3d44874adc2dfed2fde8e34bdcb5a7
KevenDuan <kevenduan@qq.com>
2024-07-30 09:29:12 +0100

    update(main.py): update walk

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def move(now_x, now_y, dist_x, dist_y):
     """
     abs_x = abs(now_x - dist_x) * 125
     abs_y = abs(now_y - dist_y) * 125
-    if abs_x < 5 and abs_y < 5: return
+    if abs_x < 8 and abs_y < 8: return True
     if dist_x > now_x:
          step.x_forward(abs_x)
     else: step.x_backward(abs_x)
@@ -26,8 +26,8 @@ def move(now_x, now_y, dist_x, dist_y):
     if dist_y > now_y:
         step.y_backward(abs_y)
     else: step.y_forward(abs_y)
-
     step.destroy()
+    return False
 
 while (True):
     # 开始用摄像头读数据，返回hx为true则表示读成功，frame为读的图像
@@ -52,11 +52,19 @@ while (True):
 
     print(point_key)
     print(point_axis)
-    machine_x, machine_y = quad_detector.detectMachineArm(frame)
-    print('axis:', (machine_x, machine_y))
-
-    if machine_x != machine_y != -1:
-        move(machine_x, machine_y, point_key[5][0], point_key[5][1])
+    while True:
+        a = int(input('input: '))
+        while True:
+            hx, frame = cap.read()
+            hx, frame = cap.read()
+            hx, frame = cap.read()
+            hx, frame = cap.read()
+            hx, frame = cap.read()
+            machine_x, machine_y = quad_detector.detectMachineArm(frame)
+            print('axis:', (machine_x, machine_y))
+            if machine_x != machine_y != -1:
+                if move(machine_x, machine_y, point_key[a][0], point_key[a][1]):
+                    break
     # black_x, black_y = quad_detector.detectBlack(frame)
     # print(black_x, black_y)
 
