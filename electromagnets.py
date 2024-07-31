3581b9a320cddded74c14c6589b6f8ac40e5e3d7
KevenDuan <kevenduan@qq.com>
2024-07-31 09:30:54 +0100

    补充电磁铁控制类

--- electromagnets.py
+++ electromagnets.py
@@ -1,22 +1,37 @@
 import RPi.GPIO as GPIO
+from time import sleep
 import sys
 
 class Electromagnets:
     def __init__(self):    
-        self.relay_pin = 11  
+        self.relay_pin = 11
         self.relay_state = False
+        self.setup()
 
+    def setup(self):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False) 
         GPIO.setup(self.relay_pin, GPIO.OUT)   
 
     def close(self):
-         GPIO.output(self.relay_pin, GPIO.LOW)
+        self.setup()
+        GPIO.output(self.relay_pin, GPIO.LOW)
+        # self.clean()
 
     def open(self):
-         GPIO.output(self.relay_pin, GPIO.HIGH)
+        self.setup()
+        GPIO.output(self.relay_pin, GPIO.HIGH)
+        # self.clean()
+
+    def clean(self):
+        GPIO.cleanup()
 
 if __name__ == "__main__":
     e = Electromagnets()
     while True:
+        # e.open()
+        # sleep(1)
+        # e.close()
+        # sleep(1)
+        # e.clean()
         e.close()
