dde59052d61d55f46fac7f1d5bf1eaff7bea8bf1
KevenDuan <kevenduan@qq.com>
2024-08-01 05:28:23 +0100

    更新了要求2的稳定性

--- main2.py
+++ main2.py
@@ -55,91 +55,34 @@ if __name__ == "__main__":
         hx, frame = cap.read()
         frame = frame[up:down, l:r]
 
-        try:
-            if len(point_axis) == 0:
-                # 四边形检测结果
-                vertices, scale_vertices, intersection = quad_detector.detect(frame) # 检测四边形方框
-                img_detected = quad_detector.draw(frame)  # 绘制检测结果
-                # 显示摄像头图像，其中的video为窗口名称，frame为图像
-                cv2.imshow('detect', img_detected)
-
-                point_axis = quad_detector.point_list
-                point_key = quad_detector.point_key
-
-            if len(black_list) == 0:
-                quad_detector.chess_detection(frame)
-                black_list = quad_detector.black_list
-
-            if len(white_list) == 0:
-                quad_detector.chess_detection(frame)
-                white_list = quad_detector.white_list
+        while len(point_axis) < 9:
+            try:
+                if len(point_axis) == 0:
+                    # 四边形检测结果
+                    vertices, scale_vertices, intersection = quad_detector.detect(frame) # 检测四边形方框
+                    img_detected = quad_detector.draw(frame)  # 绘制检测结果
+                    # 显示摄像头图像，其中的video为窗口名称，frame为图像
+                    cv2.imshow('detect', img_detected)
+                    point_axis = quad_detector.point_list
+                    point_key = quad_detector.point_key
                 
+            except Exception as e:
+                print(e)
+
+        while len(black_list) < 2:
+            quad_detector.chess_detection(frame)
             black_list = quad_detector.black_list
+
+        while len(white_list) < 2:
+            quad_detector.chess_detection(frame)
             white_list = quad_detector.white_list
-            
-        except Exception as e:
-            print(e)
         
         print(point_key) # 打印出坐标信息
-        # print(point_axis)
         
         machine_x, machine_y = quad_detector.detectMachineArm(frame)
-        print('axis:', (machine_x, machine_y))
+        print('axis:', (machine_x, machine_y)) # 打印出机械臂坐标
 
-        # 从机械臂当前位置移动到白色棋子        
-        while len(white_list) > 0 and len(point_axis) != 0:
-            
-            if white_cnt >= 2: break
-            zip_axis = white_list.pop()
-            white_x, white_y = zip_axis[0], zip_axis[1]
-            print('white_axis: ', (white_x, white_y))
-            while True: 
-                e = electromagnets.Electromagnets()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                frame = frame[up:down, l:r]
-                machine_x, machine_y = quad_detector.detectMachineArm(frame)
-                if move(machine_x, machine_y, white_x, white_y):
-                    step.down() # 落下电磁铁
-                    time.sleep(0.02)
-                    e.open() # 启动电磁铁吸取
-                    print('拿棋子成功')
-                    time.sleep(0.02)
 
-                    step.up() # 抬起电磁铁
-                    time.sleep(0.02)
-                    break
-            
-            # 从白色棋子移动到指定号九宫格
-            pos = int(input('please input postion: '))
-            while True:
-                e = electromagnets.Electromagnets()
-                e.open()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                hx, frame = cap.read()
-                frame = frame[up:down, l:r]
-                machine_x, machine_y = quad_detector.detectMachineArm(frame)
-                if move(machine_x, machine_y, point_key[pos][0], point_key[pos][1]):
-                    step.down() # 落下电磁铁
-                    time.sleep(0.02)
-                    e.close() # 释放吸力
-                    time.sleep(0.02)
-                    print('放棋子成功')
-                    step.up() # 抬起电磁铁
-                    time.sleep(0.02)
-
-                    white_cnt += 1
-
-                    break
-            
         # 从机械臂当前位置移动到黑色棋子        
         while len(black_list) > 0 and len(point_axis) != 0:
             if black_cnt >= 2: break
@@ -194,4 +137,58 @@ if __name__ == "__main__":
 
                     break
 
- 
+
+        # 从机械臂当前位置移动到白色棋子        
+        while len(white_list) > 0 and len(point_axis) != 0:
+            
+            if white_cnt >= 2: break
+            zip_axis = white_list.pop()
+            white_x, white_y = zip_axis[0], zip_axis[1]
+            print('white_axis: ', (white_x, white_y))
+            while True: 
+                e = electromagnets.Electromagnets()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                frame = frame[up:down, l:r]
+                machine_x, machine_y = quad_detector.detectMachineArm(frame)
+                if move(machine_x, machine_y, white_x, white_y):
+                    step.down() # 落下电磁铁
+                    time.sleep(0.02)
+                    e.open() # 启动电磁铁吸取
+                    print('拿棋子成功')
+                    time.sleep(0.02)
+
+                    step.up() # 抬起电磁铁
+                    time.sleep(0.02)
+                    break
+
+            
+            # 从白色棋子移动到指定号九宫格
+            pos = int(input('please input postion: '))
+            while True:
+                e = electromagnets.Electromagnets()
+                e.open()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                hx, frame = cap.read()
+                frame = frame[up:down, l:r]
+                machine_x, machine_y = quad_detector.detectMachineArm(frame)
+                if move(machine_x, machine_y, point_key[pos][0], point_key[pos][1]):
+                    step.down() # 落下电磁铁
+                    time.sleep(0.02)
+                    e.close() # 释放吸力
+                    time.sleep(0.02)
+                    print('放棋子成功')
+                    step.up() # 抬起电磁铁
+                    time.sleep(0.02)
+
+                    white_cnt += 1
+
+                    break
